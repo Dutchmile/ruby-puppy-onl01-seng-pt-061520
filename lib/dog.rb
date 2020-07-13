d7ef2648aef02639272f3dd6447d9503f81752a9
Christopher Dutcher <cadutcher@icloud.com>
2020-07-13 20:17:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,20 +17,4 @@ end
     @name = name
     Dog.save
   end
-
-  def Dog.all
-    @@all
-  end
-
-  def Dog.print_all
-    @@all.each do |dog|
-      puts "#{dog}"
-    end
-  end
-
-
-#  def Dog.clear_all
-#    @@all.each do |dog|
-#    end
-#  end
 end
