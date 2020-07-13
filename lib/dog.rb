18e8e70b72c0ff65ee81c1e4eba1ea1dd1b45a3f
Christopher Dutcher <cadutcher@icloud.com>
2020-07-13 19:45:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,19 @@
 class Dog
+attr_accessor :name
+
+@@all = []
 
 
+  def initialize(name)
+    @name = name
+  end
+
+  def Dog.all
+    @@all
+  end
+
+  def Dog.all
+    @@all.each do |dog|
+      dog.shift
+    end
 end
